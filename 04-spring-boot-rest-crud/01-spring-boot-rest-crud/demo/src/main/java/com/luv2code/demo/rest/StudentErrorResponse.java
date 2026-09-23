8e34e7af35538b45d47f939e7a0546238663602e
avishek1001 <1706avishek@gmail.com>
2026-09-24 01:48:31 +0530

    New CRUD Project - Spring Boot REST CRUD API on Employees

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/luv2code/demo/rest/StudentErrorResponse.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/luv2code/demo/rest/StudentErrorResponse.java
@@ -6,7 +6,7 @@ public class StudentErrorResponse {
     private String message;
     private long timestamp;
 
-    public StudentErrorResponse(){
+    public StudentErrorResponse() {
 
     }
 
--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -47,20 +47,5 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    // Add exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
 
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 }
